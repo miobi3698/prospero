bc7a77b555512b793ed4f9425c16ed237c1beeb5
miobi3698 <44633176+miobi3698@users.noreply.github.com>
2026-03-24 20:55:51 +0700

    changed memory from hashmap to flat array

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,28 @@
-enum Op<'a> {
+#[derive(Debug)]
+enum Op {
     VarX,
     VarY,
-    Const(f32),
-    Add(&'a str, &'a str),
-    Sub(&'a str, &'a str),
-    Mul(&'a str, &'a str),
-    Max(&'a str, &'a str),
-    Min(&'a str, &'a str),
-    Neg(&'a str),
-    Square(&'a str),
-    Sqrt(&'a str),
+    Const(f64),
+    Add(usize, usize),
+    Sub(usize, usize),
+    Mul(usize, usize),
+    Max(usize, usize),
+    Min(usize, usize),
+    Neg(usize),
+    Square(usize),
+    Sqrt(usize),
 }
 
-struct Inst<'a> {
-    addr: &'a str,
-    op: Op<'a>,
+#[derive(Debug)]
+struct Inst {
+    op: Op,
 }
 
-fn parse<'a>(source: &'a str) -> Vec<Inst<'a>> {
+fn translate_address(addr: &str) -> usize {
+    usize::from_str_radix(addr.trim_start_matches('_'), 16).unwrap()
+}
+
+fn parse(source: &str) -> Vec<Inst> {
     let mut program = Vec::new();
 
     for line in source.lines() {
@@ -26,74 +31,69 @@ fn parse<'a>(source: &'a str) -> Vec<Inst<'a>> {
         }
 
         let inst = line.split_whitespace().collect::<Vec<_>>();
-        let addr = inst[0];
-        let op = inst[1];
-
-        let op = match op {
+        let op = match inst[1] {
             "var-x" => Op::VarX,
             "var-y" => Op::VarY,
             "const" => Op::Const(str::parse(inst[2]).unwrap()),
-            "add" => Op::Add(inst[2], inst[3]),
-            "sub" => Op::Sub(inst[2], inst[3]),
-            "mul" => Op::Mul(inst[2], inst[3]),
-            "max" => Op::Max(inst[2], inst[3]),
-            "min" => Op::Min(inst[2], inst[3]),
-            "neg" => Op::Neg(inst[2]),
-            "square" => Op::Square(inst[2]),
-            "sqrt" => Op::Sqrt(inst[2]),
+            "add" => Op::Add(translate_address(inst[2]), translate_address(inst[3])),
+            "sub" => Op::Sub(translate_address(inst[2]), translate_address(inst[3])),
+            "mul" => Op::Mul(translate_address(inst[2]), translate_address(inst[3])),
+            "max" => Op::Max(translate_address(inst[2]), translate_address(inst[3])),
+            "min" => Op::Min(translate_address(inst[2]), translate_address(inst[3])),
+            "neg" => Op::Neg(translate_address(inst[2])),
+            "square" => Op::Square(translate_address(inst[2])),
+            "sqrt" => Op::Sqrt(translate_address(inst[2])),
             _ => unreachable!(),
         };
 
-        program.push(Inst { addr, op });
+        program.push(Inst { op });
     }
 
     program
 }
 
 // ported from https://github.com/processing/p5.js/blob/v1.11.11/src/math/calculation.js#L534
-fn remap_range(value: i32, start1: i32, stop1: i32, start2: i32, stop2: i32) -> f32 {
-    (value - start1) as f32 / (stop1 - start1) as f32 * (stop2 - start2) as f32 + start2 as f32
+fn remap_range(value: i64, start1: i64, stop1: i64, start2: i64, stop2: i64) -> f64 {
+    (value - start1) as f64 / (stop1 - start1) as f64 * (stop2 - start2) as f64 + start2 as f64
 }
 
 fn main() {
     let image_size = 512;
     let image_cap = (image_size * image_size) as usize;
-
     let timer = std::time::Instant::now();
     let source = std::fs::read_to_string("prospero.vm").unwrap();
     let program = parse(&source);
+
     let mut image = Vec::<u8>::with_capacity(image_cap);
-    let mut memory = std::collections::HashMap::<&str, f32>::new();
     for i in 0..image_size {
-        let y = remap_range(i, image_size, 0, -1, 1);
+        let y = remap_range(i, 0, image_size, -1, 1);
         for j in 0..image_size {
             let x = remap_range(j, 0, image_size, -1, 1);
+            let mut memory = Vec::<f64>::with_capacity(program.len());
 
             for inst in &program {
-                memory.insert(
-                    inst.addr,
-                    match inst.op {
-                        Op::VarX => x,
-                        Op::VarY => y,
-                        Op::Const(v) => v,
-                        Op::Add(a1, a2) => memory[a1] + memory[a2],
-                        Op::Sub(a1, a2) => memory[a1] - memory[a2],
-                        Op::Mul(a1, a2) => memory[a1] * memory[a2],
-                        Op::Max(a1, a2) => f32::max(memory[a1], memory[a2]),
-                        Op::Min(a1, a2) => f32::min(memory[a1], memory[a2]),
-                        Op::Neg(a) => -memory[a],
-                        Op::Square(a) => memory[a] * memory[a],
-                        Op::Sqrt(a) => f32::sqrt(memory[a]),
-                    },
-                );
+                memory.push(match inst.op {
+                    Op::VarX => x,
+                    Op::VarY => y,
+                    Op::Const(v) => v,
+                    Op::Add(a1, a2) => memory[a1] + memory[a2],
+                    Op::Sub(a1, a2) => memory[a1] - memory[a2],
+                    Op::Mul(a1, a2) => memory[a1] * memory[a2],
+                    Op::Max(a1, a2) => f64::max(memory[a1], memory[a2]),
+                    Op::Min(a1, a2) => f64::min(memory[a1], memory[a2]),
+                    Op::Neg(a) => -memory[a],
+                    Op::Square(a) => memory[a] * memory[a],
+                    Op::Sqrt(a) => f64::sqrt(memory[a]),
+                });
             }
 
-            image.push((memory[program.last().unwrap().addr] < 0.0) as u8 * 255);
+            image.push((memory[program.len() - 1] < 0.0) as u8 * 255);
             eprint!("\rProgress: {}/{}", i * image_size + j + 1, image_cap);
         }
     }
     eprintln!("\nDone in {}s.", timer.elapsed().as_secs_f64());
     // Initial implementation: 1476.815809719s
+    // Swapped to flat memory: 44.731658977s
 
     let image_data = [
         format!("P5\n{image_size} {image_size}\n255\n").as_bytes(),

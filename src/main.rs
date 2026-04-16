d52915db23028664f6be913857f5beaa3b4b0163
miobi3698 <44633176+miobi3698@users.noreply.github.com>
2026-04-16 22:45:02 +0700

    changed image chunk to flat image

--- src/main.rs
+++ src/main.rs
@@ -44,85 +44,87 @@ fn parse(source: &str) -> Vec<Inst> {
     program
 }
 
+fn exec(program: &[Inst], memory: &mut [f64], x: f64, y: f64) {
+    for (idx, inst) in program.iter().enumerate() {
+        memory[idx] = match *inst {
+            Inst::VarX => x,
+            Inst::VarY => y,
+            Inst::Const(v) => v,
+            Inst::Add(a1, a2) => memory[a1] + memory[a2],
+            Inst::Sub(a1, a2) => memory[a1] - memory[a2],
+            Inst::Mul(a1, a2) => memory[a1] * memory[a2],
+            Inst::Max(a1, a2) => f64::max(memory[a1], memory[a2]),
+            Inst::Min(a1, a2) => f64::min(memory[a1], memory[a2]),
+            Inst::Neg(a) => -memory[a],
+            Inst::Square(a) => memory[a] * memory[a],
+            Inst::Sqrt(a) => f64::sqrt(memory[a]),
+        };
+    }
+}
+
 // ported from https://github.com/processing/p5.js/blob/v1.11.11/src/math/calculation.js#L534
 fn remap_range(value: i64, start1: i64, stop1: i64, start2: i64, stop2: i64) -> f64 {
     (value - start1) as f64 / (stop1 - start1) as f64 * (stop2 - start2) as f64 + start2 as f64
 }
 
-const IMAGE_SIZE: usize = 1024;
+const IMAGE_SIZE: usize = 512;
 const IMAGE_CAP: usize = (IMAGE_SIZE * IMAGE_SIZE) as usize;
 
 fn main() {
     let source = std::fs::read_to_string("prospero.vm").unwrap();
 
     let timer = std::time::Instant::now();
+
     let program = std::sync::Arc::new(parse(&source));
+    let image = std::sync::Arc::new(std::sync::Mutex::new(vec![0 as u8; IMAGE_CAP]));
 
-    let mut coords: Vec<(f64, f64)> = Vec::with_capacity(IMAGE_CAP);
-    for i in 0..IMAGE_SIZE {
-        let y = remap_range(i as i64, 0, IMAGE_SIZE as i64, 1, -1);
-        for j in 0..IMAGE_SIZE {
-            let x = remap_range(j as i64, 0, IMAGE_SIZE as i64, -1, 1);
-            coords.push((x, y));
-        }
-    }
-
-    let thread_pool_size = std::thread::available_parallelism().unwrap().get();
+    let chunk_count = std::thread::available_parallelism().unwrap().get();
+    let chunk_size = IMAGE_CAP.div_ceil(chunk_count);
     let progress = std::sync::Arc::new(std::sync::atomic::AtomicUsize::new(0));
 
     let mut handles = Vec::new();
-    for chunk in coords.chunks(IMAGE_CAP / thread_pool_size) {
-        let chunk = chunk.to_owned();
-        let program = std::sync::Arc::clone(&program);
-        let progress = std::sync::Arc::clone(&progress);
+    for chunk_id in 0..chunk_count {
+        let program = program.clone();
+        let progress = progress.clone();
+        let image = image.clone();
         let handle = std::thread::spawn(move || {
-            let mut image: Vec<u8> = Vec::with_capacity(chunk.len());
-            let mut memory = Vec::<f64>::with_capacity(program.len());
-            for (x, y) in chunk {
-                for inst in program.iter() {
-                    memory.push(match *inst {
-                        Inst::VarX => x,
-                        Inst::VarY => y,
-                        Inst::Const(v) => v,
-                        Inst::Add(a1, a2) => memory[a1] + memory[a2],
-                        Inst::Sub(a1, a2) => memory[a1] - memory[a2],
-                        Inst::Mul(a1, a2) => memory[a1] * memory[a2],
-                        Inst::Max(a1, a2) => f64::max(memory[a1], memory[a2]),
-                        Inst::Min(a1, a2) => f64::min(memory[a1], memory[a2]),
-                        Inst::Neg(a) => -memory[a],
-                        Inst::Square(a) => memory[a] * memory[a],
-                        Inst::Sqrt(a) => f64::sqrt(memory[a]),
-                    });
-                }
+            let mut memory = vec![0.0; program.len()];
+            let low = chunk_id * chunk_size;
+            let high = ((chunk_id + 1) * chunk_size).min(IMAGE_CAP);
+            for idx in low..high {
+                let x = idx % IMAGE_SIZE;
+                let y = idx / IMAGE_SIZE;
+                let vy = remap_range(y as i64, 0, IMAGE_SIZE as i64, 1, -1);
+                let vx = remap_range(x as i64, 0, IMAGE_SIZE as i64, -1, 1);
 
-                image.push((memory[program.len() - 1] < 0.0) as u8 * 255);
+                exec(&program, &mut memory, vx, vy);
+                image.lock().unwrap()[y * IMAGE_SIZE + x] =
+                    (*memory.last().unwrap() < 0.0) as u8 * 255;
                 let count = progress.fetch_add(1, std::sync::atomic::Ordering::Relaxed) + 1;
                 eprint!("\rProgress: {}/{} pixels", count, IMAGE_CAP);
-                memory.clear();
             }
-
-            image
         });
         handles.push(handle);
     }
 
-    let mut image = Vec::new();
     for handle in handles {
-        image.extend_from_slice(&handle.join().unwrap());
+        handle.join().unwrap();
     }
+
     eprintln!("\nDone in {}s.", timer.elapsed().as_secs_f64());
 
     // 512x512
     // Initial implementation: 1476.815809719s
     // Swapped to flat memory: 44.731658977s
     // Moved to multithreading: 7.466684195s
+    // Swapped to flat image: 6.111408442s
 
     // 1024x1024
     // Multithreading: 30.092374533s
 
     let image_data = [
         format!("P5\n{IMAGE_SIZE} {IMAGE_SIZE}\n255\n").as_bytes(),
-        image.as_slice(),
+        image.lock().unwrap().as_slice(),
     ]
     .concat();
     std::fs::write("out.ppm", image_data).unwrap();
